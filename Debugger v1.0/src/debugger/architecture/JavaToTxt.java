f8879c38d9a66a45b1ffc72e927d8afcaa72809e
Esteban <estebandcg1999@gmail.com>
2017-10-27 18:30:32 -0600

    [Representación del código]

--- Debugger v1.0/src/debugger/architecture/JavaToTxt.java
+++ Debugger v1.0/src/debugger/architecture/JavaToTxt.java
@@ -4,15 +4,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 public class JavaToTxt {
-	public static void main (String[] args){
-		String file = "C:\\Users\\este0\\eclipse-workspace\\Lectura\\src\\Clase2.java";
+	public static void addCode(String clase){
+		String file = "C:\\Users\\este0\\eclipse-workspace\\Debugger\\src\\debugger\\architecture\\"+clase+".java";
 		String outputFile = String.format("%stxt", file.replace(".java", "."));
 		try (BufferedReader br = new BufferedReader(new FileReader(file));
 				PrintWriter pw = new PrintWriter(new FileWriter(outputFile))){
 			String line;
 			while ((line = br.readLine()) != null){
 				if (!(line.equals(""))){
-					line = line.replaceAll("\t","");
 					pw.printf(line);
 					pw.printf("%n");
 					System.out.println(line);
