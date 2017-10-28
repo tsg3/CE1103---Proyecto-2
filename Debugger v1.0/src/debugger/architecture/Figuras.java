5d2363c4a0b2759171ce0b61e9ed7eaac6f3be78
Esteban <estebandcg1999@gmail.com>
2017-10-27 18:31:54 -0600

    [Localización del diagrama en el JFrame]

--- Debugger v1.0/src/debugger/architecture/Figuras.java
+++ Debugger v1.0/src/debugger/architecture/Figuras.java
@@ -16,8 +16,8 @@ public class Figuras extends JPanel{
 	public static String[] texto;
 
 	public void create(Graphics g, int altura, String linea){
-		g.drawString(linea,200-10*(linea.length()/4)-20 + (this.getWidth()-2*(200-10*(linea.length()/4)-20) - g.getFontMetrics(g.getFont()).stringWidth(linea)) / 2, altura - 20 + ((30 - g.getFontMetrics(g.getFont()).getHeight()) / 2) + g.getFontMetrics(g.getFont()).getAscent());
-		g.drawOval(200-10*(linea.length()/4)-20,altura-20, this.getWidth()-2*(200-10*(linea.length()/4)-20),30);
+		g.drawString(linea,400+415-10*(linea.length()/4)-20 + (this.getWidth()-2*(415-10*(linea.length()/4)-20)-400 - g.getFontMetrics(g.getFont()).stringWidth(linea)) / 2, altura - 20 + ((30 - g.getFontMetrics(g.getFont()).getHeight()) / 2) + g.getFontMetrics(g.getFont()).getAscent());
+		g.drawOval(400+380-10*(linea.length()/4)-20,altura-20, this.getWidth()-2*(380-10*(linea.length()/4)-20)-400,30);
 	}
 
 	public void paint(Graphics g){
@@ -39,17 +39,17 @@ public class Figuras extends JPanel{
 			create(g,altura,texto[i]);
 			if (!(i==texto.length-1)){
 				//linea que une grafico
-				g.drawLine(250, altura+10, 250, altura+30);
+				g.drawLine(400+380-10*(texto[i].length()/4)-20+(this.getWidth()-2*(380-10*(texto[i].length()/4)-20)-400)/2, altura+10, 400+380-10*(texto[i].length()/4)-20+(this.getWidth()-2*(380-10*(texto[i].length()/4)-20)-400)/2, altura+30);
 				//linea izquierda que une ciclos
-				g.drawLine(200-10*(texto[i].length()/4)-20, altura-5, 200-10*(texto[i].length()/4)-20-30, altura-5);
+				/*g.drawLine(200-10*(texto[i].length()/4)-20, altura-5, 200-10*(texto[i].length()/4)-20-30, altura-5);
 				g.drawLine(200-10*(texto[i].length()/4)-20-30, altura-5, 200-10*(texto[i].length()/4)-20-30, altura+80);
 				g.drawLine(200-10*(texto[i].length()/4)-20-30, altura+80, 250, altura+80);
-				g.drawLine(250, altura+80, 250, altura+60);
+				g.drawLine(250, altura+80, 250, altura+60);*/
 				//linea derecha que une ciclos
-				g.drawLine(200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20), altura+45, 200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20)+30, altura+45);
+				/*g.drawLine(200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20), altura+45, 200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20)+30, altura+45);
 				g.drawLine(200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20)+30, altura+45, 200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20)+30, altura+90);
 				g.drawLine(200-10*(texto[i+1].length()/4)-20+this.getWidth()-2*(200-10*(texto[i+1].length()/4)-20)+30, altura+90, 250, altura+90);
-				g.drawLine(250, altura+90, 250, altura+110);
+				g.drawLine(250, altura+90, 250, altura+110);*/
 			}
 			altura+=50;
 			i++;
@@ -92,7 +92,7 @@ public class Figuras extends JPanel{
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(new Figuras());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
+		frame.setSize(1300,700);
 		frame.setVisible(true);
 	}
 }
