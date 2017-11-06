77a26fb2810b550723b407c5d2dcd85d64781825
Esteban <estebandcg1999@gmail.com>
2017-11-05 19:35:44 -0600

    [Interfaz 1.0]

--- Debugger v1.0/src/debugger/architecture/SingleGraphics.java
+++ Debugger v1.0/src/debugger/architecture/SingleGraphics.java
@@ -3,6 +3,7 @@ package debugger.architecture;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.util.Stack;
 
 import javax.swing.JPanel;
 
@@ -11,6 +12,7 @@ public class SingleGraphics extends JPanel {
 	public ListaVariables vars = new ListaVariables();
 	public Diagrama diagram = new Diagrama();
 	public String clase;
+	public Stack<Integer> stack = new Stack<Integer>();
 
 	public SingleGraphics(String clase){
 		this.clase = clase;
@@ -76,15 +78,43 @@ public class SingleGraphics extends JPanel {
 		g.setFont(new Font("TimesRoman",Font.PLAIN,20));
 		g.setColor(Color.BLUE);
 		i=0;
+		int j=0;
+		int lineaLarga=diagram.lineaLarga();
 		while (i<diagram.largo){
 			g.drawString(diagram.figura(i).getLine(),400+415-10*(diagram.figura(i).getLine().length()/4)-20 + (this.getWidth()-2*(415-10*(diagram.figura(i).getLine().length()/4)-20)-400 - g.getFontMetrics(g.getFont()).stringWidth(diagram.figura(i).getLine())) / 2,diagram.figura(i).getY() - 20 + ((30 - g.getFontMetrics(g.getFont()).getHeight()) / 2) + g.getFontMetrics(g.getFont()).getAscent()+30);
 			g.drawOval(400+380-10*(diagram.figura(i).getLine().length()/4)-20,diagram.figura(i).getY()+10,this.getWidth()-2*(380-10*(diagram.figura(i).getLine().length()/4)-20)-400,30);
-			if (diagram.figura(i+1)!=null)
+			if (diagram.figura(i+1)!=null){
 				g.drawLine(400+380-10*(diagram.figura(i).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(i).getLine().length()/4)-20)-400)/2, diagram.figura(i).getY()+40, 400+380-10*(diagram.figura(i).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(i).getLine().length()/4)-20)-400)/2, diagram.figura(i).getY()+60);
+				if (diagram.figura(i+1).getY()==(i+1)*50+(j+1)*30){
+					int startLine = stack.pop();
+					int finalLine = i;
+
+					if ((diagram.figura(startLine).getLine().contains("while"))||(diagram.figura(startLine).getLine().contains("for"))){
+						g.drawLine(400+380-10*(diagram.figura(startLine).getLine().length()/4)-20, diagram.figura(startLine).getY()+25, 400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20-30-(j*30), diagram.figura(startLine).getY()+25);
+						g.drawLine(400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20-30-(j*30), diagram.figura(startLine).getY()+25, 400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20-30-(j*30), diagram.figura(finalLine).getY()+60);
+						g.drawLine(400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20-30-(j*30), diagram.figura(finalLine).getY()+60, 400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+60);
+
+						g.drawLine(400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400), diagram.figura(startLine).getY()+25, 400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(startLine).getY()+25);
+						g.drawLine(400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(startLine).getY()+25, 400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(finalLine).getY()+70);
+						g.drawLine(400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(finalLine).getY()+70, 400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+70);
+						g.drawLine(400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+70, 400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+90);
+					}
+
+					else if (diagram.figura(startLine).getLine().contains("if")){
+						g.drawLine(400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400), diagram.figura(startLine).getY()+25, 400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(startLine).getY()+25);
+						g.drawLine(400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(startLine).getY()+25, 400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(finalLine).getY()+60);
+						g.drawLine(400+380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(lineaLarga).getLine().length()/4)-20)-400)+30+(j*30), diagram.figura(finalLine).getY()+60, 400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+60);
+						g.drawLine(400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+60, 400+380-10*(diagram.figura(startLine).getLine().length()/4)-20+(this.getWidth()-2*(380-10*(diagram.figura(startLine).getLine().length()/4)-20)-400)/2, diagram.figura(finalLine).getY()+90);
+					}
+					j++;
+				}
+			}
+			if ((diagram.figura(i).getLine().contains("if"))||(diagram.figura(i).getLine().contains("for"))||(diagram.figura(i).getLine().contains("while"))){
+				stack.push(i);
+			}
 			i++;
 		}
 
+
 	}
-
-
 }
